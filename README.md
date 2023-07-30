4533e249c81a08fbc07147dce89708d266a20c2e
Pedro-V <mingas62@hotmail.com>
2023-07-30 18:34:06 -0300

    added .gitconfig to linkmaker

--- README.md
+++ README.md
@@ -2,8 +2,11 @@
 
 Configs for my Unix experience, by theme.
 
+In case you want to make symlinks, the `link_maker.py` script automatically
+makes some of them.
+
 **WARNING:** Some configurations depend on the environment. For example, [my `vimrc`](editor/vim/vimrc)
 uses [`minpac`](https://github.com/k-takata/minpac) as package manager. This depends on having
 the source tree of `minpac` in the appropriate place. Sometimes I look for a more robust
 configuration code. However this is not alway possible, or I simply don't have the
-knowledge. Therefore, read carefully!
+knowledge. It may be helpful to clone the repo with `--recurse-submodules`.
---
+++ link_maker.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""
+Symlinks for dotfiles.
+"""
+import os
+import pathlib
+
+def prompt_delete_file(file):
+    while True:
+        choice = input(f"Destination path '{file}' already exists. Do you want to delete it? (y/n): ")
+        if choice.lower() in ['y', 'yes']:
+            return True
+        elif choice.lower() in ['n', 'no']:
+            return False
+        else:
+            print("Invalid choice. Please enter 'y' or 'n'.")
+
+def make_symlink(origin, dest):
+    try:
+        os.symlink(origin, dest)
+        print(f"Symbolic link created: {dest} -> {origin}")
+    except FileExistsError:
+        if prompt_delete_file(dest):
+            try:
+                os.remove(dest)
+                os.symlink(origin, dest)
+                print(f"Symbolic link created: {dest} -> {origin}")
+            except Exception as e:
+                print(f"Error creating symlink: {e}")
+        else:
+            print("Symlink creation aborted.")
+
+SCRIPT_DIR = pathlib.Path(__file__).resolve().parent
+HOME_DIR = pathlib.Path.home()
+
+# TODO look for a more robust, less verbose way of doing this
+
+symlink_paths = {
+    "shell/bashrc":     ".bashrc",
+    "shell/gitconfig":  ".gitconfig",
+    "editor/vim":       ".vim",
+    "gui/i3":           ".config/i3",
+}
+
+for origin in symlink_paths:
+    make_symlink(SCRIPT_DIR / origin,
+                 HOME_DIR / symlink_paths[origin])
+
